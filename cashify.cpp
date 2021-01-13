bd4186fc844bc617814862d5b6308cec34a4ac78
dhairya16 <dhairyasavlekar99@outlook.com>
2021-01-13 17:17:59 +0530

    Indented

--- cashify.cpp
+++ cashify.cpp
@@ -4,64 +4,64 @@ using namespace std;
 
 int main() {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
 
-	int no_of_transactions,friends;
-	cin>>no_of_transactions>>friends;
+	int no_of_transactions, friends;
+	cin >> no_of_transactions >> friends;
 
-	int x,y,amount;
+	int x, y, amount;
 
 	// Make a 1-D array to store the net amount that each person will have to take at the end
-	int net[100000]={0};
+	int net[100000] = {0};
 
-	while(no_of_transactions--) {
-		cin>>x>>y>>amount;
-		net[x]-=amount;
-		net[y]+=amount;
+	while (no_of_transactions--) {
+		cin >> x >> y >> amount;
+		net[x] -= amount;
+		net[y] += amount;
 	}
 
 	multiset<int> m;
 	// Init a list & sort it => multiset
-	for(int i=0;i<friends;i++) {
-		if(net[i]!=0) {
+	for (int i = 0; i < friends; i++) {
+		if (net[i] != 0) {
 			m.insert(net[i]);
 		}
 	}
 
 	// Pop out two person (left & right) and try to settle them
-	int cnt=0;
-	while(!m.empty()) {
-		auto low=m.begin();
-		auto high=prev(m.end());
+	int cnt = 0;
+	while (!m.empty()) {
+		auto low = m.begin();
+		auto high = prev(m.end());
 
-		int debit=*low;
-		int credit=*high;
+		int debit = *low;
+		int credit = *high;
 
 		// Erase
 		m.erase(low);
 		m.erase(high);
 
 		// Settlement
-		int settlement_amount=min(-debit,credit);
+		int settlement_amount = min(-debit, credit);
 
 		cnt++;
 
 		// Settlement_amount from donor to acceptor
-		debit+=settlement_amount;
-		credit-=settlement_amount;
+		debit += settlement_amount;
+		credit -= settlement_amount;
 
 		// one of them will be zero
-		if(debit!=0) {
+		if (debit != 0) {
 			m.insert(debit);
 		}
-		if(credit!=0) {
+		if (credit != 0) {
 			m.insert(credit);
 		}
 	}
 
-	cout<<cnt<<endl;
+	cout << cnt << endl;
 
 	return 0;
 }
